a9bd82a411806e4a857b22f6c6d6b9b49c098e6f
ajiteshshukla <ajiteshshukla75@gmail.com>
2015-10-01 19:12:28 +0530

    adding final fragments and changes in Main Activity onResume

--- dodapp/src/main/java/com/lphaindia/dodapp/dodapp/ScreenSlidePageFragment3.java
+++ dodapp/src/main/java/com/lphaindia/dodapp/dodapp/ScreenSlidePageFragment3.java
@@ -2,6 +2,7 @@ package com.lphaindia.dodapp.dodapp;
 
 import android.content.Context;
 import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
 import android.preference.PreferenceActivity;
 import android.support.v4.app.Fragment;
@@ -9,6 +10,10 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
+import com.facebook.drawee.backends.pipeline.Fresco;
+import com.facebook.drawee.interfaces.DraweeController;
+import com.facebook.drawee.view.DraweeView;
+import com.facebook.drawee.view.SimpleDraweeView;
 
 /**
  * Created by ajitesh.shukla on 9/10/15.
@@ -20,6 +25,14 @@ public class ScreenSlidePageFragment3 extends Fragment implements View.OnClickLi
                              Bundle savedInstanceState) {
         ViewGroup rootView = (ViewGroup) inflater.inflate(
                 R.layout.fragment_screen_slide_page3, container, false);
+        DraweeView draweeView = (SimpleDraweeView) rootView.findViewById(R.id.accessibilitygif_holder);
+        DraweeController controller = Fresco.newDraweeControllerBuilder()
+                .setUri(Uri.parse("res:///" + R.drawable.enable_accessibility_demo))
+                .setAutoPlayAnimations(true)
+                .build();
+        draweeView.setController(controller);
+        draweeView.setMaxWidth(200);
+        draweeView.setMaxHeight(320);
         Button nextButton = (Button) rootView.findViewById(R.id.cont);
         nextButton.setOnClickListener(this);
         return rootView;
